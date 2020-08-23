99cbe2160fd71dc5239dad741b8a3c9bdb731ccc
Ivy <ivy83718@gmail.com>
2020-08-23 20:41:15 +0800

    week8訂正

--- homeworks/week8/hw1/set.js
+++ homeworks/week8/hw1/set.js
@@ -8,56 +8,56 @@ document.querySelector('.button').addEventListener('click', () => {
       const newBg = document.querySelector('.lotterys');
       if (prizename === 'FIRST') {
         newBg.innerHTML = `
-        <div class = "first">
-        <div class = "prize">
-          <div class = "prize__desc">
+        <div class="first">
+        <div class="prize">
+          <div class="prize__desc">
             恭喜你中頭獎了！日本東京來回雙人遊！
           </div>
-          <div class = "button">
-            <input type = "button" value = "我要抽獎"/>
+          <div class="button">
+            <input type="button" value="我要抽獎"/>
           </div>
         </div>
         </div>`;
       } else if (prizename === 'SECOND') {
         newBg.innerHTML = `
-        <div class = "second">
-        <div class = "prize">
-          <div class = "prize__desc">
+        <div class="second">
+        <div class="prize">
+          <div class="prize__desc">
             二獎！90 吋電視一台！
           </div>
-          <div class = "button">
-            <input type = "button" value = "我要抽獎"/>
+          <div class="button">
+            <input type="button" value="我要抽獎"/>
           </div>
         </div>
         </div>`;
       } else if (prizename === 'THIRD') {
         newBg.innerHTML = `
-        <div class = "third">
-        <div class = "prize">
-          <div class = "prize__desc">
+        <div class="third">
+        <div class="prize">
+          <div class="prize__desc">
             恭喜你抽中三獎：知名 YouTuber 簽名握手會入場券一張，bang！
           </div>
-          <div class = "button">
-            <input type = "button" value = "我要抽獎"/>
+          <div class="button">
+            <input type="button" value="我要抽獎"/>
           </div>
         </div>
         </div>`;
       } else if (prizename === 'NONE') {
         newBg.innerHTML = `
-        <div class = "none">
-          <div class = "prize">
-            <div class = "prize__desc">
+        <div class="none">
+          <div class="prize">
+            <div class="prize__desc">
               銘謝惠顧
             </div>
-            <div class = "button">
-            <input type = "button" value = "我要抽獎"/>
+            <div class="button">
+            <input type="button" value="我要抽獎"/>
             </div>
           </div>
           </div>`;
       } else {
         alert('系統不穩定，請再試一次');
       }
-      document.querySelector('input').addEventListener('click', () => {
+      document.querySelector('.button').addEventListener('click', () => {
         window.location.reload();
       });
     }
--- homeworks/week8/hw2/set.js
+++ homeworks/week8/hw2/set.js
@@ -1,106 +1,72 @@
-const gameList = () => {
+const gameList = (cb) => {
   const request = new XMLHttpRequest();
   const navList = document.querySelector('.navbar__list');
-  const content = document.querySelector('.games_streams');
-  const title = document.querySelector('h2');
-  request.open('GET', 'https://api.twitch.tv/kraken/games/top?_limit=5', true);
+  request.open('GET', 'https://api.twitch.tv/kraken/games/top?limit=5', true);
   request.setRequestHeader('Client-ID', 'z85tx0z4lzuytncvoj61aewdhzqsk3');
   request.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
   request.onload = () => {
     const response = request.responseText;
     const data = JSON.parse(response).top;
     if (request.status >= 200 && request.status < 400) {
-      for (let i = 0; i < 5; i += 1) {
+      for (let i = 0; i < data.length; i += 1) {
         const li = document.createElement('li');
-        li.innerText = `${data[i].game.name}`;
+        li.innerText = data[i].game.name;
         navList.appendChild(li);
       }
-
-      const newReq = new XMLHttpRequest();
-      newReq.open('GET', `https://api.twitch.tv/kraken/streams/?game=${data[0].game.name}`, true);
-      newReq.setRequestHeader('Client-ID', 'z85tx0z4lzuytncvoj61aewdhzqsk3');
-      newReq.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
-      newReq.onload = () => {
-        if (newReq.status >= 200 && newReq.status < 400) {
-          const newresponse = newReq.responseText;
-          const newdata = JSON.parse(newresponse).streams;
-          title.innerText = `${data[0].game.name}`;
-          for (let i = 0; i < 20; i += 1) {
-            const div = document.createElement('div');
-            div.classList.add('game');
-            div.innerHTML = `
-            <a href = ${newdata[i].channel.url} />
-              <img class = "game__video" src = ${newdata[i].preview.medium} ></img>
-              <div class = "game__info">
-                <img class = "game__pic" src = ${newdata[i].channel.logo}></img>
-                <div class = "game__name">
-                  ${newdata[i].channel.status}
-                <p class = "game__kol">${newdata[i].channel.name}</p>
-                </div>
-              </div>`;
-            content.appendChild(div);
-          }
-          for (let i = 0; i < 2; i += 1) {
-            const empty = document.createElement('div');
-            empty.classList.add('game');
-            content.appendChild(empty);
-          }
-        }
-      };
-      newReq.send();
+      cb();
     }
   };
   request.send();
 };
 
-const gameLive = () => {
+const gameLive = (n) => {
   const newReq = new XMLHttpRequest();
   const content = document.querySelector('.games_streams');
   const title = document.querySelector('h2');
-  document.querySelector('.navbar').addEventListener('click', (e) => {
-    let gameName = '';
-    if (e.target.innerHTML !== '') {
-      gameName = e.target.innerHTML;
-    }
-    newReq.open('GET', `https://api.twitch.tv/kraken/streams/?game=${gameName}`, true);
-    newReq.setRequestHeader('Client-ID', 'z85tx0z4lzuytncvoj61aewdhzqsk3');
-    newReq.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
-    newReq.onload = () => {
-      if (newReq.status >= 200 && newReq.status < 400) {
-        content.innerHTML = '';
-        const response = newReq.responseText;
-        const newdata = JSON.parse(response).streams;
-        title.innerText = `${gameName}`;
-        for (let i = 0; i < 20; i += 1) {
-          const div = document.createElement('div');
-          div.classList.add('game');
-          div.innerHTML = `
-          <a href =${newdata[i].channel.url}/>
-            <img class = "game__video" src = ${newdata[i].preview.medium}></img>
-            <div class = "game__info">
-              <img class = "game__pic" src =${newdata[i].channel.logo}></img>
-              <div class = "game__name">
-                ${newdata[i].channel.status}
-              <p class = "game__kol">${newdata[i].channel.name}</p>
-              </div>
-            </div>`;
-          content.appendChild(div);
-        }
-        for (let i = 0; i < 2; i += 1) {
-          const empty = document.createElement('div');
-          empty.classList.add('game');
-          content.appendChild(empty);
-        }
-      } else {
-        console.log(newReq.status, newReq.responseText);
+  let gameName = document.querySelector('li').innerText;
+  if (n) {
+    gameName = n;
+  }
+  newReq.open('GET', `https://api.twitch.tv/kraken/streams/?game=${gameName}`, true);
+  newReq.setRequestHeader('Client-ID', 'z85tx0z4lzuytncvoj61aewdhzqsk3');
+  newReq.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
+  newReq.onload = () => {
+    if (newReq.status >= 200 && newReq.status < 400) {
+      content.innerHTML = '';
+      const response = newReq.responseText;
+      const newdata = JSON.parse(response).streams;
+      title.innerText = gameName;
+      for (let i = 0; i < 20; i += 1) {
+        const div = document.createElement('div');
+        div.classList.add('game');
+        div.innerHTML = `
+        <a href=${newdata[i].channel.url}/>
+          <img class="game__video" src=${newdata[i].preview.medium}></img>
+          <div class="game__info">
+            <img class="game__pic" src=${newdata[i].channel.logo}></img>
+            <div class="game__name">
+              ${newdata[i].channel.status}
+            <p class="game__kol">${newdata[i].channel.name}</p>
+            </div>
+          </div>`;
+        content.appendChild(div);
       }
-    };
-    newReq.onerror = () => {
-      console.log('error');
-    };
-    newReq.send();
-  });
+      for (let i = 0; i < 2; i += 1) {
+        const empty = document.createElement('div');
+        empty.classList.add('game');
+        content.appendChild(empty);
+      }
+    } else {
+      console.log(newReq.status, newReq.responseText);
+    }
+  };
+  newReq.onerror = () => {
+    console.log('error');
+  };
+  newReq.send();
 };
 
-gameList();
-gameLive();
+gameList(gameLive);
+document.querySelector('.navbar').addEventListener('click', (e) => {
+  gameLive(e.target.innerHTML);
+});
