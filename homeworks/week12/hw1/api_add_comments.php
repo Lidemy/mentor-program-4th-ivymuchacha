2e87d0262bb0bf4c992032a05ece71c9073d6a31
Ivy <ivy83718@gmail.com>
2020-09-29 12:24:29 +0800

    hw1

---
+++ homeworks/week12/hw1/api_add_comments.php
@@ -0,0 +1,46 @@
+<?php 
+  require_once('conn.php');
+  header('Content-type:application/json;charset=utf-8');
+  header('Access-Control-Allow-Origin:*');
+
+  if (empty($_POST['site_key'])||
+      empty($_POST['nickname'])||
+      empty($_POST['content'])
+  ) {
+      $json = array(
+        "ok"=>false,
+        "message"=>"Please insert the empty fields"
+      );
+
+      $response = json_encode($json);
+      echo $response;
+      die();
+  }
+
+  $site_key = $_POST['site_key'];
+  $nickname = $_POST['nickname'];
+  $content = $_POST['content'];
+
+  $sql = "INSERT INTO ivymuchacha_w12_comments(site_key, nickname, content) VALUES(?, ?, ?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('sss', $site_key, $nickname, $content);
+  $result=$stmt->execute();
+  
+  if(!$result) {
+    $json = array(
+      "ok"=>false,
+      "message"=>$conn->error
+    );
+
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+
+  $json = array(
+    "ok" => true,
+    "message" =>"sucess"
+  );
+  $response = json_encode($json);
+  echo $response;
+?>
